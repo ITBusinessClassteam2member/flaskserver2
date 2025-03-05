86568ddfd4a2517833cbc779f5cf99995128381b
unknown <33061@wind.iwaki-cc.ac.jp>
2025-03-05 09:59:38 +0900

    Initial commit

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+
+# if __name__ == "__main__":
+#     app.run(port=10000, debug=True)
+#------------------------------------------------------------
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# 環境変数 PORT を取得（デフォルトは 4000）
+port = int(os.getenv("PORT", 4000))
+
+@app.route("/")
+def home():
+    return "Hello World!"
 
 if __name__ == "__main__":
-    app.run(port=10000, debug=True)
+    app.run(host="0.0.0.0", port=port)
